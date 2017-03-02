e71c3fea879304c96bdc976cd8182784f339beb8
Bhargavi Bakhle <bhargavi@Bhargavis-MacBook-Air.local>
2017-03-03 01:19:57 +0530

    React Routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Home from './Home';
 import './index.css';
 
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={Home}></Route>
+    <Route path="/display" component={App}></Route>
+  </Router>,
   document.getElementById('root')
 );
