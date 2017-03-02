d27122bd7204564db1812ec1fa6305a4c7a25a36
Bhargavi Bakhle <bhargavi@Bhargavis-MacBook-Air.local>
2017-03-02 22:15:40 +0530

    Todo list done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Stocks from './Stocks';
-import Weather from './Weather';
 import News from './News';
+import Stocks from './Stocks';
+import Todos from './Todos';
+import Weather from './Weather';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Stocks />
+        <Todos />
         <Weather />
         <News />
       </div>
---
+++ src/Todos.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import Request from 'superagent';
+import _ from 'lodash';
+
+import './Todos.css';
+
+class Todos extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentWillMount() {
+    this.fetchTodos(); // only for first time. when page loads to perform search
+  };
+
+  componentDidMount() {
+    setInterval (() => {
+      this.fetchTodos();
+    }, 1000 * 5);
+  };
+
+  componentWillReceiveProps(newProps) {
+  };
+
+  shouldComponentUpdate(newProps, newState) {
+    return true;
+  };
+
+  componentWillUpdate(nextProps, nextState) {
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+  };
+
+  componentWillUnmount() {
+  };
+
+
+  fetchTodos(){
+    const serverUrl = 'http://localhost:5000/users/todos';
+    Request.get(serverUrl)
+      .then((response) => {
+        this.setState({
+          todos: response.body
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  render() {
+    const stockList = _.map(this.state.todos, (todo) => {
+      if (todo.status == 'false') {  //  display only the ones which are not completed 
+        return(
+          <li className="todo" key={todo.text}>
+            <label><input className="checkbox" type="checkbox" value="" disabled />{todo.text}</label>
+          </li>
+        );
+      }
+    });
+
+    return (
+      <div className='Todos'>
+        <div className='container'>
+          <ul>{stockList}</ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Todos;
