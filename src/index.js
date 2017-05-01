5840b653a758bfd92cb9f010db2808bf16044703
Aaron Colaco <aaroncolaco.work@gmail.com>
2017-05-01 23:50:14 +0530

    Change react-router to v3.0.5

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Display from './display/Display';
 import Home from './home/Home';
-import Todo from './todo/Todo'
+import Todo from './todo/Todo';
 import './index.css';
 
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 
 
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
       <Route path="display" component={Display}></Route>
